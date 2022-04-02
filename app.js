b6dac7fae28b58e2b313d81e95a535a1d2547e19
ndavidj <ndavidj@hotmail.com.ar>
2022-04-02 13:29:20 -0300

    Se agrega archivo app.js; y las carpetas public y views con sus detalles

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+let publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(4000, () => {
+    console.log('Servidor corriendo en el puerto http://localhost:4000')
+})
+
+// ----- HOME -----
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
+
+// ----- LOGIN -----
+app.get('/login', (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
+})
+
+// ----- REGISTRO -----
+app.get('/registro', (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'/views/registro.html'))
+})
+
+// ----- DETALLE DEL PRODUCTO -----
+app.get("/detalle-del-producto", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/detalle-del-producto.html"));
+  });
+
+// ----- CARRITO DE COMPRAS -----
+app.get('/carrito-de-compras', (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'./views/carrito-de-compras.html'))
+})
+
+
+
