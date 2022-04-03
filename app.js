10bc8b672192efc79c3818b28c966ad20dfeed8d
ndavidj <ndavidj@hotmail.com.ar>
2022-04-03 19:57:46 -0300

    comenzando pruebas sobre el header

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ app.listen(4000, () => {
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname,'./views/home.html'))
 })
+app.get('/home', (req, res) =>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+})
 
 // ----- LOGIN -----
 app.get('/login', (req, res) =>{
