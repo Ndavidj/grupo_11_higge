e2fba07de552d15bf84d82637577ff2719580042
ndavidj <ndavidj@hotmail.com.ar>
2022-04-13 20:25:01 -0300

    corrección en app.js

--- app.js
+++ app.js
@@ -1,46 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-let publicPath = path.resolve(__dirname, './public');
+let publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.listen(4000, () => {
-    console.log('Servidor corriendo en el puerto http://localhost:4000')
-})
+app.listen(process.env.PORT || 4000, () => {
+  console.log("Servidor corriendo en el puerto http://localhost:4000");
+});
 
 // ----- HOME -----
-app.get('/', (req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./views/home.html'))
-})
-app.get('/home', (req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./views/home.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+app.get("/home", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
 
 // ----- LOGIN -----
-app.get('/login', (req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./views/login.html'))
-})
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
 
 // ----- REGISTRO -----
-app.get('/registro', (req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./views/registro.html'))
-})
+app.get("/registro", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/registro.html"));
+});
 
 // ----- CATÁLOGO -----
 app.get("/catalogo", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/catalogo.html"));
-  });
-  
+  res.sendFile(path.resolve(__dirname, "./views/catalogo.html"));
+});
+
 // ----- DETALLE DEL PRODUCTO -----
 app.get("/detalle-del-producto", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/detalle-del-producto.html"));
-  });
+  res.sendFile(path.resolve(__dirname, "./views/detalle-del-producto.html"));
+});
 
 // ----- CARRITO DE COMPRAS -----
-app.get('/carrito-de-compras', (req, res) =>{
-    res.sendFile(path.resolve(__dirname,'./views/carrito-de-compras.html'))
-})
-
-
-
+app.get("/carrito-de-compras", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/carrito-de-compras.html"));
+});
