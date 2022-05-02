8c700174cdd8554723ac3d6933314de1d6d8f0b0
ndavidj <ndavidj@hotmail.com.ar>
2022-05-01 21:04:35 -0300

    renderizando otras vistas

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,6 +2,10 @@ const mainController = {
   index: (req, res) => {
     res.render("index");
   },
+
+  aboutUs: (req, res) => {
+    res.render('aboutUs')
+  }
 };
 
 module.exports = mainController;
--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -5,6 +5,7 @@ const mainController = require("../controllers/mainController.js")
 
 
 router.get('/', mainController.index);
+router.get('/aboutUs', mainController.aboutUs);
 
 
 module.exports = router;
