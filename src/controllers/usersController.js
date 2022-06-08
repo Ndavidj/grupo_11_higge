dbdb98eb3ec9e7c1a49512a406fada2971369320
sebasperey <spereyrairaola@gmail.com>
2022-06-08 09:54:09 -0300

    implemente el registro de usuarios con multer, la ruta por post y el controller para el store al registrarse e hice los ajustes para que funcione en el archivo ejs register

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -16,6 +16,29 @@ const usersController = {
   contact: (req, res) => {
     res.render("users/contact");
   },
-};
+
+  // store con la lógica para que guarde la info del nuevo usuario al registrarse
+  store: (req, res)=> {
+    let newUser = {
+      id: users[users.length - 1].id + 1,
+      name: req.body.name,
+      userName: req.body.userName,
+      email: req.body.email,
+      birthdate: Date(req.body.birthdate),
+      address: req.body.address,
+      interest: req.body.interest,
+      password: req.body.password,
+      image: req.file ? req.file.filename : "userDefaultImage.png",
+    };
+
+    users.push(newUser);
+
+    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "));
+
+    res.redirect('/login');
+
+  },
+  }
+;
 
 module.exports = usersController;
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const path = require('path'); 
 const multer = require('multer');
-const usersController = require("../controllers/usersController.js")
+const usersController = require("../controllers/usersController.js");
+
+// Config's Multer 
+const userStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../public/images/users/userImage'))
+    },
+    filename: function (req, file, cb) {
+        const userImg = 'user-' + file.fieldname + Date.now() + path.extname(file.originalname)
+        cb(null, userImg);
+    }
+});
+
+const upload = multer ({storage: userStorage});
 
 
 router.get('/login', usersController.login);
 router.get('/register', usersController.register);
-router.post('/register', fileUpload.single('userImage'), usersController.store);
+router.post('/register', usersController.store);
 router.get('/contact', usersController.contact);
 
 
