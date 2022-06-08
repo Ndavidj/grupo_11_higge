e260b6083af2373923ba78609510e77a60480d59
sebasperey <spereyrairaola@gmail.com>
2022-06-08 08:18:28 -0300

    configure la libreria multer para aplicarla al registro de usuarios

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+//Configuro a continuación el archivo de usuarios de la base de datos de Json
+const usersFilePath = path.join(__dirname, '../data/usersData.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
 const usersController = {
   login: (req, res) => {
     res.render("users/login");
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
+const multer = require('multer');
 const usersController = require("../controllers/usersController.js")
 
 
 router.get('/login', usersController.login);
 router.get('/register', usersController.register);
+router.post('/register', fileUpload.single('userImage'), usersController.store);
 router.get('/contact', usersController.contact);
 
 
