3d5d1599d13313113e67993f9efb837eece58e5b
ndavidj <ndavidj@hotmail.com.ar>
2022-08-03 21:50:39 -0300

    Se crea nuevamente la carpeta db

---
+++ db/create_higgedb.sql
@@ -0,0 +1,56 @@
+DROP DATABASE IF EXISTS `higge_db`;
+CREATE DATABASE IF NOT EXISTS `higge_db`;
+USE `higge_db`;
+
+CREATE TABLE `categories` (
+   `id` INT,
+   `category` VARCHAR(100),
+   PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `users` (
+   `id` INT AUTO_INCREMENT,
+   `firstName` VARCHAR(50) NOT NULL,
+   `lastName` VARCHAR(255) NOT NULL,
+   `email` VARCHAR(40) NOT NULL,
+   `dateBirthday` DATE,
+   `address` VARCHAR(255),
+   `interest` VARCHAR(100),
+   `avatar` VARCHAR(255),
+   `password` VARCHAR(255) NOT NULL,
+   `roleId` INT,
+   PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `products` (
+   `id` INT NOT NULL,
+   `name` VARCHAR(60) NOT NULL,
+   `description` VARCHAR(255) NOT NULL,
+   `price` INT NOT NULL,
+   `discount` TINYINT,
+   `image` VARCHAR(100),
+   `categoryId` INT NOT NULL,
+   PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `roles` (
+   `id` INT AUTO_INCREMENT,
+   `role` VARCHAR(100) NOT NULL,
+   PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `users_products` (
+   `id` INT,
+   `userId` INT NOT NULL,
+   `productsId` INT NOT NULL,
+   PRIMARY KEY (`id`)
+);
+
+
+ALTER TABLE `users` ADD CONSTRAINT `FK_ee5a67c5-923a-4187-8b99-b9254c9002b3` FOREIGN KEY (`roleId`) REFERENCES `roles`(`id`)  ;
+
+ALTER TABLE `products` ADD CONSTRAINT `FK_b121f266-be89-4884-b54f-84c12253cb96` FOREIGN KEY (`categoryId`) REFERENCES `categories`(`id`)  ;
+
+ALTER TABLE `users_products` ADD CONSTRAINT `FK_a9349f79-eaef-4f8f-be9f-adb975f77eda` FOREIGN KEY (`userId`) REFERENCES `users`(`id`)  ;
+
+ALTER TABLE `users_products` ADD CONSTRAINT `FK_def08fc7-e324-4c42-9649-642b3161e532` FOREIGN KEY (`productsId`) REFERENCES `products`(`id`)  ;
---
+++ db/populate_higge.sql
@@ -0,0 +1,45 @@
+------------------------------------------------ erase everything ------------------------------------------------
+
+-- products --
+DELETE FROM higge_db.products;
+ALTER TABLE higge_db.products AUTO_INCREMENT=1;
+
+-- users --
+DELETE FROM higge_db.users;
+ALTER TABLE higge_db.users AUTO_INCREMENT=1;
+
+-- categories --
+DELETE FROM higge_db.categories;
+ALTER TABLE higge_db.categories AUTO_INCREMENT=1;
+
+-- roles --
+DELETE FROM higge_db.roles;
+ALTER TABLE higge_db.roles AUTO_INCREMENT=1;
+
+
+
+------------------------------------------------ populate everything ------------------------------------------------
+
+-- categories --
+INSERT INTO higge_db.categories (id, category) VALUES
+(1, "Cine"), (2, "Música"), (3, "Naturaleza"), (4, "Arte"), (5, "Deportes");
+
+
+-- roles --
+INSERT INTO higge_db.roles (id, role) VALUES
+(1, "Admin"), (2, "Cliente");
+
+-- products --
+INSERT INTO higge_db.products (id, name, description, price, discount, image, categoryId) VALUES 
+(1, "Pulp Fiction", "Behind The Scenes", 12000, 10, "escenaPulpFiction.jpg", "2"),
+(2, "Home Alone", "Kevin en problemas", 12000, 10, "macaulayCulkin.jpg", "2"),
+(3, "Jack Nicholson", "Sonrie Jack!", 1200, 10, "jackNicholson.jpg", "2"),
+(4, "Jules & Vincent", "Que dupla", 9000, 10, "jules&Vincent.jpg", "2"),
+(5, "Jaws", "steven Spielberg y el Tiburón", 12000, 15, "stevenSpielberg.jpg", "2"),
+(6, "Harry", "Mutt Cutts", 9000, 15, "harryDunne.jpg", "2");
+
+-- users --
+INSERT INTO higge_db.users (id, firstName, lastName, email, dateBirthday, address, interest, avatar, password, roleId) VALUES 
+(1, "Admin", "Higge", "admin@higge.com.ar", "1991-01-01", 'asd 123', 'Cine', "admin.jpg", "$2a$12$0a.pwY7sEmoEOJfXmvR6du7frjT8CfzVvWMBSTXzEee6G2mM2TYta", 1),
+(2, "Cliente", "Higge", "cliente@higge.com.ar", "1990-01-01", 'asd 123', 'Deportes', "cliente.jpg", "$2a$12$/iEIHkW756A025eKoD8PD.xyHVsohSoEgBiD61Q.sDxIc0bLyC56i", 2)
+
